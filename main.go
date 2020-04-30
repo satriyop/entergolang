3eb6ec16daf45be8519629c05b2291bba1056221
Satriyo Pamungkas <satriyopamungkas@gmail.com>
2020-04-30 23:11:19 +0700

    add gorilla mux and scaffold dir

--- main.go
+++ main.go
@@ -4,16 +4,20 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
 	fmt.Println("Entergolang Start")
+	r := mux.NewRouter()
+	r.HandleFunc("/", indexHandler)
 
-	http.Handle("/", http.HandlerFunc(index))
+	http.Handle("/", r)
 
 	log.Fatal(http.ListenAndServe(":9900", nil))
 }
 
-func index(w http.ResponseWriter, r *http.Request) {
+func indexHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "EnterGolang")
 }
---
+++ note.md
@@ -0,0 +1,30 @@
+Entergolang is blog
+
+### Route and Handler
+#### Public Route
+##### Index
+Path : /
+Handler : indexHandler
+Result : 
+- Will display landing page to visitors
+Template : 
+
+
+##### Posts
+- Path : /posts
+- Result : 
+    - Will display all posts stored in DB
+
+
+
+##### Individual Post
+- Path : /posts/{postId}
+- Result : 
+    - Will display individual post to visitor
+
+
+#### Private Route
+/login
+/logout
+/dashboard
+
